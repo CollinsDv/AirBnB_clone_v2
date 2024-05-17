95313704e0f34b1a6bc565c18d1ac50437a42138
Collins Kimani <mwas9446.ck@gmail.com>
2024-05-17 11:36:18 +0300

    add web_flask folder

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module: 0-hello_route
+"""
+from flask import Flask
+
+# instanciate a Flask instance with module name
+app = Flask(__name__)
+
+# decorator to register view function to URL rulw
+@app.route('/', strict_slashes=False)
+def hello():
+    """return a string on route invoke
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ web_flask/README.md
@@ -0,0 +1,2 @@
+# Web_flask
+This is a practice exercise enriching one with proper knowledge over Flask web framework, integrating it with the Airbnb_v2 clone
