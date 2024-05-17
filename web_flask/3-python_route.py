1d801937aa17c50e30e8f03b24f4e73e6a7d91b2
Collins Kimani <mwas9446.ck@gmail.com>
2024-05-17 16:45:13 +0300

    add file 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+module: 1-hbnb_route
+"""
+from flask import Flask
+from markupsafe import escape
+
+# instanciate a Flask instance with module name
+app = Flask(__name__)
+
+
+# decorator to register view function/controller to URL rule
+@app.route('/', strict_slashes=False)
+def hello():
+    """return a string on route path "/hbnb" invoke
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hnb():
+    """return a string on route path "/hbnb" invoke
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """return string in "/c/is-fun"
+       replace "_" with space
+    """
+    # replace '_'
+    text = text.replace('_', ' ')
+    return f'C {escape(text)}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+def python_route(text):
+    """implement a default variable entry
+    """
+    text = text.replace('_', ' ')
+    return f'Python {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
